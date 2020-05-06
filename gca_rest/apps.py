888f8ec5e6940f1aea9281bd4f3dbe3ac565b8d7
Áron Ricardo Perez-Lopez <perez.aron@gmail.com>
2020-05-06 17:27:25 -0400

    Add REST app

---
+++ gca_rest/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GcaRestConfig(AppConfig):
+    name = 'gca_rest'
--- geogebra_classroom_activities/settings.py
+++ geogebra_classroom_activities/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gca_rest.apps.GcaRestConfig',
 ]
 
 MIDDLEWARE = [
