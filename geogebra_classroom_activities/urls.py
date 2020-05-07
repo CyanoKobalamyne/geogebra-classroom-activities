6c5b4836f12910624148fed0c2ab5d39de467825
Áron Ricardo Perez-Lopez <perez.aron@gmail.com>
2020-05-06 22:54:56 -0400

    Add URL mappings for teacher app

--- geogebra_classroom_activities/urls.py
+++ geogebra_classroom_activities/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ggca_rest.urls')),
+    path('teacher/', include('ggca_teacher.urls')),
 ]
