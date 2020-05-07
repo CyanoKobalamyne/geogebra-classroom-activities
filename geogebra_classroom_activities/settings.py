c19c256a3db1d64f623c5a46663e81f2804701f9
Áron Ricardo Perez-Lopez <perez.aron@gmail.com>
2020-05-06 20:13:07 -0400

    Add rest framework to apps

--- geogebra_classroom_activities/settings.py
+++ geogebra_classroom_activities/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'gca_rest.apps.GcaRestConfig',
 ]
 
